6b4332f0e6ce5cac1feba84a35c81aa7dfc31cb7
hyebin <sksp0401@gmail.com>
2024-08-23 14:25:25 +0900

    Add: icons 추가

---
+++ public/icons/index.ts
@@ -0,0 +1,6 @@
+import PenIcon from "./pen-solid.svg";
+import TrashIcon from "./trash-solid.svg";
+import CheckedIcon from "./circle-check-solid.svg";
+import NotCheckedIcon from "./circle-check-regular.svg";
+
+export { PenIcon, TrashIcon, CheckedIcon, NotCheckedIcon };
